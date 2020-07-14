bab431bdce482c20d2c55acd10a823f07230c459
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2020-07-14 11:46:04 -0300

    Alterar rotina de geração do arquivo CSV
    Verificar se o arquivo retornado tem HIST no nome (formato histórico). Se não for no formato histórico, compara os cabeçalhos para tentar mesclar os dados. Se não, deixa o arquivo novo à parte.
    
    O Ministério da Saúde mudou, no dia 13/07/2020, o arquivo retornado de `HIST_PAINEL_COVIDBR_%dd%mmm%yyyy` para `HOJE_PAINEL_COVIDBR_%dd%mmm%yyyy`, removendo as colunas `casosNovos` e `obitosNovos`.

--- gerar-csv.sh
+++ gerar-csv.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 JSON=portal.json
-CSV=covid19-br.csv
+CSV_HIST=covid19-br.csv
 
 echo "Fazendo download do arquivo JSON"
 curl -# "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral" -H "X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm" --compressed -o $JSON
 
+XLSX_URL=$(jq -r '.results[0].arquivo.url' $JSON)
+XLSX_FILENAME=$(jq -r '.results[0].arquivo.name' $JSON)
+
 echo "Fazendo download do arquivo XLSX"
-curl -# -O -J $(jq -r '.results[0].arquivo.url' $JSON)
+curl -# -O -J $XLSX_URL
 
 echo "Convertendo o XLSX em CSV"
-xlsx2csv -f "%Y-%m-%d" $(jq -r '.results[0].arquivo.name' $JSON) $CSV
+xlsx2csv -f "%Y-%m-%d" $XLSX_FILENAME $XLSX_FILENAME.csv
+
+DELETE_XLSXCSV=1
+
+if [[ $XLSX_FILENAME == *"HIST"* ]]; then
+    echo "> HIST"
+    cat $XLSX_FILENAME.csv > $CSV_HIST
+else
+    echo "> HOJE"
+    if [[ $(head -1 $CSV_HIST) == $(head -1 $XLSX_FILENAME.csv) ]]; then
+        tail -n+2 $XLSX_FILENAME.csv >> $CSV_HIST
+    else
+        echo "> O cabeçalho dos arquivos CSV estão diferentes. Não será possível mesclar conteúdo no arquivo principal"
+        DELETE_XLSXCSV=0
+    fi
+fi
+
+[ $DELETE_XLSXCSV -eq 1 ] && rm $XLSX_FILENAME.csv
