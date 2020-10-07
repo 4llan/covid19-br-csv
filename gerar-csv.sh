e5da6db5579ba43c895fc87496e9d51730a5678c
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2020-10-06 22:34:37 -0300

    Alterar rotina de geração do arquivo para verificar origem em ZIP ou CSV
    Ministério da Saúde está alternando a disponobilização do arquivo público nestes dois formatos.
    Só falta voltarem a publicar um arquivo em XLSX novamente. :)

--- gerar-csv.sh
+++ gerar-csv.sh
@@ -5,8 +5,20 @@ CSV_HIST=covid19-br.csv
 echo "Fazendo download do arquivo JSON"
 curl -# "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral" -H "X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm" --compressed -o $JSON
 
-echo "Fazendo download do arquivo ZIP"
+PORTAL_ARQUIVO=$(jq -r '.results[0].arquivo.name' $JSON)
+
+echo "Fazendo download do arquivo"
 curl -# -O -J $(jq -r '.results[0].arquivo.url' $JSON)
 
-echo "Descompactando o arquivo CSV"
-unzip -p $(jq -r '.results[0].arquivo.name' $JSON) > $CSV_HIST
+if [[ $PORTAL_ARQUIVO == *".zip" ]]; then
+    echo "Descompactando o arquivo CSV"
+    unzip -p $PORTAL_ARQUIVO > $CSV_HIST
+else
+    if [[ $PORTAL_ARQUIVO == *".csv" ]]; then
+        echo "Substituindo o arquivo CSV"
+        cat $PORTAL_ARQUIVO > $CSV_HIST
+        rm $PORTAL_ARQUIVO
+    else
+        echo "Pela 300a vez no ano, o Ministério da Saúde mudou o formato do dado disponibilizado"
+    fi
+fi
