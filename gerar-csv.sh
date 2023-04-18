d52caf96659a23759e37d30073d1a5ef85cbf378
Allan M. de Azevedo <4llan@users.noreply.github.com>
2023-04-18 19:23:43 +0000

    Add proxy variable

--- gerar-csv.sh
+++ gerar-csv.sh
@@ -2,16 +2,17 @@
 JSON=portal.json
 CSV_HIST=covid19-br.csv
 updatedAt=last-update.txt
+proxy="http://201.91.82.155:3128"
 
 echo "Fazendo download do arquivo JSON"
-curl -# "https://qd28tcd6b5.execute-api.sa-east-1.amazonaws.com/prod/PortalGeral" -H "X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm" --compressed -o $JSON
+curl -# --proxy $proxy "https://qd28tcd6b5.execute-api.sa-east-1.amazonaws.com/prod/PortalGeral" -H "X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm" --compressed -o $JSON
 
 [ $(jq -r '.results[0].updatedAt' $JSON) == $(cat $updatedAt) ] && cat $updatedAt && exit 0
 
 PORTAL_ARQUIVO=$(jq -r '.results[0].arquivo.name' $JSON)
 
 echo "Fazendo download do arquivo"
-curl -f -# -O --proxy "http://201.91.82.155:3128" -J $(jq -r '.results[0].arquivo.url' $JSON)
+curl -f -# -O --proxy $proxy -J $(jq -r '.results[0].arquivo.url' $JSON)
 [ $? -ne 0 ] && exit 1
 
 [ -f "$CSV_HIST.gz" ] && gzip -d -c $CSV_HIST.gz > $CSV_HIST
